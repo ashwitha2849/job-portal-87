a425813914e1b4f391112d429c79be1915ff3391
ashwitha2849 <ashwithat2004@gmail.com>
2026-06-03 16:58:54 +0530

    Commit

--- server/check-companies.js
+++ server/check-companies.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import Company from './models/Company.js';
+import dns from 'dns';
 
 dotenv.config();
 
 const check = async () => {
   try {
+    dns.setServers(["8.8.8.8", "1.1.1.1"]);
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
     const companies = await Company.find({}, 'name email password');
